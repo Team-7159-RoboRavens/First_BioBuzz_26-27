e2b27f900389c2fc5c2006729898b5963bf03922
oversizedFart <84299941+hiPeeps124816@users.noreply.github.com>
2026-08-31 17:27:43 -0700

    PedroPathingBase

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Essentials/Constants.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Essentials;
+
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.ftc.localization.constants.TwoWheelConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.135, 0, 0.011, 0.012))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.76, 0, 0.02, 0.055))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1,0.0,0.01,0.4,0.05))
+            .forwardZeroPowerAcceleration(-60.14)
+            .lateralZeroPowerAcceleration(-61.79)
+            .mass(9);
+
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0.9, 0.9);
+
+
+    //This is where the magic happens baby!
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(mecanumConstants)
+                .build();
+    }
+
+    public static MecanumConstants mecanumConstants = new MecanumConstants()
+            .maxPower(1)
+            .xVelocity(65.4)
+            .yVelocity(45)
+            .rightFrontMotorName("rightFront")
+            .leftFrontMotorName("leftFront")
+            .rightRearMotorName("rightBack")
+            .leftRearMotorName("leftBack")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+
+    //This is just instantiating the normal driving
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(2.5)
+            .strafePodX(-7.5)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+//    public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
+//            .forwardEncoder_HardwareMapName("rightFront")
+//            .forwardEncoderDirection(Encoder.FORWARD)
+//            .forwardPodY(-6.1)
+//            .strafeEncoder_HardwareMapName("leftFront")
+//            .strafeEncoderDirection(Encoder.REVERSE)
+//            .strafePodX(3.6)
+//            .IMU_HardwareMapName("imu")
+//            .IMU_Orientation(
+//                    new RevHubOrientationOnRobot(
+//                            RevHubOrientationOnRobot.LogoFacingDirection.UP,
+//                            RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+//                    )
+//            );
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PedroPathingBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PedroPathingBase.java
@@ -1,6 +1,62 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-public class PedroPathingBase {
+import org.firstinspires.ftc.teamcode.Autonomous.Essentials.Constants;
+
+@Autonomous(name="PedroPathingBase")
+public class PedroPathingBase extends OpMode {
+    private Follower follower;
+
+    public enum PathState {
+        START_POS,
+        END_POS
+    }
+
+    PathState pathState;
+    private final Pose startPose = new Pose(0, 0, 0);
+    private final Pose endPose = new Pose(20, 20, Math.PI/2);
+
+    private PathChain chain1;
+
+    public void buildPaths() {
+        chain1 = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, endPose))
+                .build();
+    }
+
+    public void updateStates() {
+        switch (pathState) {
+            case START_POS:
+                follower.followPath(chain1, true);
+                pathState = PathState.END_POS;
+                break;
+            case END_POS:
+                if (!follower.isBusy()) {
+                    telemetry.addLine("finished chain1");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    public void init() {
+        pathState = PathState.START_POS;
+        follower = Constants.createFollower(hardwareMap);
+
+        buildPaths();
+        follower.setPose(startPose);
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        updateStates();
+    }
 }
